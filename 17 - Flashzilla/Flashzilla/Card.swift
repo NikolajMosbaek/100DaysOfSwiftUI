30dbf215adbfb4a6a268cda33e9b1b0466d8926a
Nikolaj Simonsen <nikolajmos@me.com>
2022-04-14 22:59:49 +0200

    Updated project 17

--- 17 - Flashzilla/Flashzilla/Card.swift
+++ 17 - Flashzilla/Flashzilla/Card.swift
@@ -7,7 +7,7 @@
 
 import Foundation
 
-struct Card {
+struct Card: Codable {
 	let promt: String
 	let answer: String
 	
--- 17 - Flashzilla/Flashzilla/CardView.swift
+++ 17 - Flashzilla/Flashzilla/CardView.swift
@@ -9,33 +9,78 @@ import SwiftUI
 
 struct CardView: View {
 	let card: Card
+	var removal: (() -> Void)? = nil
 	
+	@State private var feedBack = UINotificationFeedbackGenerator()
+	
+	@Environment(\.accessibilityDifferentiateWithoutColor) var differentiateWithoutColor
+	@Environment(\.accessibilityVoiceOverEnabled) var voiceOverEnabled
 	@State private var isShowingAnswer = false
+	@State private var offset = CGSize.zero
 	
     var body: some View {
 		ZStack {
 			RoundedRectangle(cornerRadius: 25, style: .continuous)
-				.fill(.white)
+				.fill(
+					differentiateWithoutColor
+					? .white
+					: .white.opacity(1 - Double(abs(offset.width / 50)))
+				)
+				.background(
+					differentiateWithoutColor
+					? nil
+					: RoundedRectangle(cornerRadius: 25, style: .continuous)
+						.fill(offset.width > 0 ? .green : .red)
+				)
 				.shadow(radius: 10)
 			
 			VStack {
-				Text(card.promt)
-					.font(.largeTitle)
-					.foregroundColor(.black)
-				
-				if isShowingAnswer {
-					Text(card.answer)
-						.font(.title)
-						.foregroundColor(.gray)
+				if voiceOverEnabled {
+					Text(isShowingAnswer ? card.answer : card.promt)
+						.font(.largeTitle)
+						.foregroundColor(.black)
+				} else {
+					Text(card.promt)
+						.font(.largeTitle)
+						.foregroundColor(.black)
+					
+					if isShowingAnswer {
+						Text(card.answer)
+							.font(.title)
+							.foregroundColor(.gray)
+					}
 				}
 			}
 			.padding()
 			.multilineTextAlignment(.center)
 		}
 		.frame(width: 450, height: 250)
+		.rotationEffect(.degrees(Double(offset.width / 5)))
+		.offset(x: offset.width * 5, y: 0)
+		.opacity(2 - Double(abs(offset.width / 50)))
+		.accessibilityAddTraits(.isButton)
+		.gesture(
+			DragGesture()
+				.onChanged { gesture in
+					offset = gesture.translation
+					feedBack.prepare()
+				}
+				.onEnded { _ in
+					if abs(offset.width) > 100 {
+						if offset.width < 0 {
+							feedBack.notificationOccurred(.error)
+						}
+						
+						removal?()
+					} else {
+						offset = .zero
+					}
+				}
+		)
 		.onTapGesture {
 			isShowingAnswer.toggle()
 		}
+		.animation(.spring(), value: offset)
     }
 }
 
